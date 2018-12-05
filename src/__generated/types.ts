856f929ad0e1d3df4ff2a44cb12ca49ca7d9a6a9
kro <kai.roeder@aeb.com>
2018-12-05 11:20:17 +0100

    initial

---
+++ src/__generated/types.ts
@@ -0,0 +1,12 @@
+/** A list of options for the sort order of the feed */
+export enum FeedType {
+  Hot = "HOT",
+  New = "NEW",
+  Top = "TOP"
+}
+/** The type of vote to record, when submitting a vote */
+export enum VoteType {
+  Up = "UP",
+  Down = "DOWN",
+  Cancel = "CANCEL"
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import gql from 'graphql-tag';
+
+const outsideFragmentWorks = gql`
+  fragment VoteFields on Vote {
+    vote_value
+  }
+`;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-gql-gen-issue';
+
+  thisFragmentWorks = outsideFragmentWorks;
+  orThisOneAsWell = gql`
+    ${outsideFragmentWorks}
+  `;
+
+  // > gql-gen
+  //
+  // √ Parse configuration
+  // > Generate outputs
+  // > Generate src/__generated/types.ts
+  // √ Load GraphQL schemas
+  // √ Load GraphQL documents
+  // × Generate
+  // → Found 2 errors in your documents
+  //
+  //
+  //   We found 1 errors
+  //
+  //   Failed to generate src/__generated/types.ts
+  //
+  //   Found 2 errors.
+  //   GraphQL Code Generator validated your GraphQL documents against the schema.
+  //   Please fix following errors and run codegen again:
+  //
+  // ./src/app/app.component.ts:
+  //   There can be only one fragment named "VoteFields".
+  //
+  // ./src/app/app.component.ts:
+  //   There can be only one fragment named "VoteFields".
+  thisFragmentDoesNotWork = gql`
+    fragment VoteFields on Vote {
+      vote_value
+    }
+  `;
 }
